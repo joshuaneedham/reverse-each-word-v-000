f19c779b68b67de4d1ced85c34eda2d7eb6cb623
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 21:05:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(sentence)
   words = sentence.split(" ")
   reverse_words = []
   words.collect { |word| reverse_words << word.reverse }
-  reverse_words.join(", ")
+  reverse_words.join(" ")
 end
