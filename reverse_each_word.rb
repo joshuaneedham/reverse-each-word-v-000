05bd2c7fb2d9b45122454cf84f511365732af05b
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 20:30:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_sentence = []
   sentence.collect do |words|
-    new_sentence << words.to_s.reverse.
+    new_sentence << words.to_s.reverse
   end
   new_sentence
 end
