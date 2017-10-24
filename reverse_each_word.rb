9432581c8a97d243bd5e16ab44f18198046a2a9f
Joshua Needham <joshuabneedham@gmail.com>
2017-10-24 20:36:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(sentence)
   new_sentence = []
-  sentence.map { |words| "#{words}.reverse" }
-  new_sentence << "#{words}"
+  sentence.map do |words| new_sentence << "#{words}"
+  new_sentence.reverse
+  end
+  new_sentence.
 end
